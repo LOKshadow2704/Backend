ef288c08bd1b5211f2d3cc45d50f2f0bf2fe8747
LOKshadow2704 <nguyenlocface@gmail.com>
2024-12-11 23:57:09 +0700

    .

--- controllers/controll_orderProduct.php
+++ controllers/controll_orderProduct.php
@@ -249,9 +249,7 @@ class controll_Order extends Control
                     //     $cart->deleteItem($item['IDSanPham'], $cusID);
                     // }
                 } elseif ($result["status"] == "CANCELLED") {
-                    $this->model_order->
                     $this->model_order->delete_order($payment_data["data"]["orderCode"]);
-                    $this->model_product->minute_one();
                 }
                 http_response_code(200);
                 echo json_encode(['status' => $result["status"]]);
